0bddea0df7e3595f51d35a891f57a2f557c47e73
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-06-25 15:13:23 -0500

    Add layout component, greeting section

---
+++ 99_project_blog_app/components/home-page/hero.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import styles from './hero.module.css';
+
+function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image src="/images/site/emoji.png" width={300} height={300} alt="An image" />
+      </div>
+      <h1>Hi, I'm Wonjun</h1>
+      <p>I blog about web development - especially frontend frameworks like React</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ 99_project_blog_app/components/layout/logo.js
@@ -0,0 +1,7 @@
+import styles from './logo.module.css';
+
+function Logo() {
+  return <div className={styles.logo}>Jun's Next Blog</div>;
+}
+
+export default Logo;
