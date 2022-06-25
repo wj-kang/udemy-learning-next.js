6a160372f52bd59bb4986f78df2dc5d85eb817bd
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-06-25 11:30:28 -0500

    Add notification - utilizing useContext() hook

--- 05_api_routes/components/input/newsletter-registration.js
+++ 05_api_routes/components/input/newsletter-registration.js
@@ -1,14 +1,22 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Sigining up...',
+      message: 'Registering for Newsletter',
+      status: 'pending',
+    });
 
     fetch('/api/newsletter', {
       method: 'POST',
@@ -17,8 +25,28 @@ function NewsletterRegistration() {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        response.json().then((result) => {
+          throw new Error(result.message);
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Successfully registered for newsletter!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
@@ -26,13 +54,7 @@ function NewsletterRegistration() {
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-            ref={emailInputRef}
-          />
+          <input type="email" id="email" placeholder="Your email" aria-label="Your email" ref={emailInputRef} />
           <button>Register</button>
         </div>
       </form>
