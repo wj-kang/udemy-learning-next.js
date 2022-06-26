52553f1a93a485520bac89dbdc66872fffa01290
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-06-25 23:01:03 -0500

    Render markdown using react-markdown library / render code snippet with syntax highlighting

---
+++ 99_project_blog_app/components/posts/post-detail/post-content.js
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
+import PostHeader from './post-header';
+import styles from './post-content.module.css';
+// import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+        return (
+          <div className={styles.image}>
+            <Image //
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      } else {
+        return <p>{paragraph.children}</p>;
+      }
+    },
+
+    code(code) {
+      const { className, children } = code;
+      // className is something like language-js => We need the "js" part here
+      const language = className.split('-')[1];
+      return <SyntaxHighlighter style={materialDark} language={language} children={children} />;
+    },
+  };
+
+  return (
+    <article className={styles.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
+    </article>
+  );
+}
+
+export default PostContent;
