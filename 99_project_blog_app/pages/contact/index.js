cbfbf71ee149c7234641d0b678f806024f609b17
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-06-25 23:21:57 -0500

    Contact page

---
+++ 99_project_blog_app/pages/contact/index.js
@@ -0,0 +1,11 @@
+import ContactForm from '../../components/contact/contact-form';
+
+function ContactPage() {
+  return (
+    <>
+      <ContactForm />
+    </>
+  );
+}
+
+export default ContactPage;
